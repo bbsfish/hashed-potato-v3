d546c5c30897573bcbc2221a711cc511455aef24
YSK <98343699+bbsfish@users.noreply.github.com>
2024-10-01 17:37:25 +0900

    Update client

--- Client/src/components/DecryptFileButton.vue
+++ Client/src/components/DecryptFileButton.vue
@@ -23,22 +23,43 @@ export default {
       // Empty チェック
       if (this.$store.getters['datastore/isEmptyData']) {
         this.$log.info('This file has no data.');
-        this.$emit('ondecrypted', await this.$store.getters['datastore/editorState']);
+        this.$emit(
+          'ondecrypted',
+          await this.$store.getters['datastore/editorState'],
+        );
         return;
       }
       // 複合化
-      const password = await this.$dialog.prompt({ message: 'Enter PASSPHRASE for Data Decrypt' });
-      try {
-        await this.$store.dispatch('datastore/decryptContent', { password });
-        this.$emit('ondecrypted', await this.$store.getters['datastore/editorState']);
-      } catch (error) {
-        this.$log.error('[DecryptFileButton]', error);
-      }
+      let resuming = true;
+      /* eslint-disable no-await-in-loop */
+      do {
+        const password = await this.$dialog.prompt({
+          message: 'Enter PASSPHRASE for Data Decrypt',
+        });
+        try {
+          await this.$store.dispatch('datastore/decryptContent', { password });
+          this.$emit(
+            'ondecrypted',
+            await this.$store.getters['datastore/editorState'],
+          );
+          return;
+        } catch (error) {
+          this.$log.error('[DecryptFileButton]', error);
+          if (
+            !(await this.$dialog.confirm({ message: '複合化に失敗しました. 再入力しますか？' }))
+          ) {
+            resuming = false;
+            this.$router.go({
+              path: this.$router.currentRoute.path,
+              force: true,
+            });
+          }
+        }
+      } while (resuming);
     },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-
 </style>
